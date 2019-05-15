3c7527c4a772bbeca698f01c5f131254ef45d645
lai <lai@qq.com>
2019-05-15 17:05:38 +0800

    第一次架构提交

---
+++ src/main/java/com/lz/demo/GitDemo.java
@@ -0,0 +1,9 @@
+package  com.lz.demo;
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("zhengg");
+        for (String arg : args) {
+            
+        }
+    }
+}
