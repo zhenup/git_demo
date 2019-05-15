1f646dd31c2bd7be2a91046c09c94218364a0058
lai <lai@qq.com>
2019-05-15 17:10:48 +0800

    第一次修改

--- src/main/java/com/lz/demo/GitDemo.java
+++ src/main/java/com/lz/demo/GitDemo.java
@@ -2,8 +2,6 @@ package  com.lz.demo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("zhengg");
-        for (String arg : args) {
-            
-        }
+        System.out.println("hello github");
     }
 }
